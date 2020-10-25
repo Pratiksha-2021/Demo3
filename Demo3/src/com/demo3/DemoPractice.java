09f48d9d5d7c9b655050b3e54082fc51c2c42dff
snehalphatangare <snehalphatangare@gmail.com>
2020-10-25 17:43:01 +0530

    commit first

---
+++ Demo3/src/com/demo3/DemoPractice.java
@@ -0,0 +1,13 @@
+
+package com.demo3;
+
+public class DemoPractice {
+	public void m1() {
+		System.out.println("hi");
+	}
+
+	public static void main(String[] args) {
+		DemoPractice dd = new DemoPractice();
+		dd.m1();
+	}
+}
